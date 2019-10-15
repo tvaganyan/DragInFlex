903711e5f9597bfc8963fa997720fddb746237b8
tvaganyan <tvaganyan@yandex.ru>
2019-10-15 22:06:27 +0300

    Drag in flex html5

---
+++ js/dragInFlex.js
@@ -0,0 +1,51 @@
+﻿        function dragInit(D, callbackF) {
+            var o;
+            function allowDrop(ev) {
+                ev.preventDefault();
+            }
+
+            function drag(ev) {
+                o = ev.target;
+            }
+
+            function drop(ev) {
+                ev.preventDefault();
+                var target = ev.target;
+                while (target) {
+                    if (target.dataset && parseInt(target.dataset.n,10) >= 0) {
+                        if (parseInt(o.dataset.n,10) > parseInt(target.dataset.n,10)) {
+                            D.insertBefore(o, target);
+                        } else {
+                            D.insertBefore(o, target.nextSibling);
+                        }
+                        setOrder();
+                        if (callbackF) {
+                            callbackF(D);
+                        }
+                        break;
+                    } else {
+                        target = target.parentNode;
+                    }
+                }
+            }
+            function setOrder() {
+                var f = D.children;
+                for (var i = 0; i < f.length; i++) {
+                    f[i].dataset.n = i;
+                }
+            }
+            setOrder();
+            var f = D.children;
+            for (var i = 0; i < f.length; i++) {
+                f[i].setAttribute("draggable", "true");
+                f[i].addEventListener("dragstart", function (ev) {
+                    drag(ev);
+                });
+                f[i].addEventListener("drop", function (ev) {
+                    drop(ev);
+                });
+                f[i].addEventListener("dragover", function (ev) {
+                    allowDrop(ev);
+                });
+            }
+        } 
---
+++ js/dragInFlex2.js
@@ -0,0 +1,50 @@
+﻿   function dragInit(d_out, d_in, callbackF) {
+            var o;
+            function allowDrop(ev) {
+                ev.preventDefault();
+            }
+
+            function drag(ev) {
+                o = ev.target;
+            }
+
+            function drop(ev) {
+                ev.preventDefault();
+                var target = ev.target;
+                while (target) {
+                    if (target == d_in || target == d_out) {
+                        target.appendChild(o);
+                        if(callbackF) {
+                           callbackF(d_out, d_in);
+                        }
+                        break;
+                    } else {
+                        target = target.parentNode;
+                    }
+                }
+            }
+
+            function childDrag(d) {
+                var f = d.children;
+                for (var i = 0; i < f.length; i++) {
+                    f[i].setAttribute("draggable", "true");
+                    f[i].addEventListener("dragstart", function (ev) {
+                        drag(ev);
+                    });
+                    f[i].addEventListener("drop", function (ev) {
+                        drop(ev);
+                    });
+                    f[i].addEventListener("dragover", function (ev) {
+                        allowDrop(ev);
+                    });
+                }
+                d.addEventListener("drop", function (ev) {
+                    drop(ev);
+                });
+                d.addEventListener("dragover", function (ev) {
+                    allowDrop(ev);
+                });
+            }
+            childDrag(d_out);
+            childDrag(d_in);
+        } 
